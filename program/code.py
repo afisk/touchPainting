112f27d35a7639cfb1a02d7859f1269ec793765b
afisk <fiskaustin@rocketmail.com>
2022-06-21 20:07:48 -0500

    Fix for setting to less sensitive before sleep

--- program/code.py
+++ program/code.py
@@ -281,7 +281,7 @@ except Exception as e:
 print("Going to sleep")
 
 # Set Touch sensor to be less sensitive while asleep
-mpr121.write_register_byte(mpr121.MPR121_TOUCHTH_0 + 2 * i, 25)
+mpr121._write_register_byte(adafruit_mpr121.MPR121_TOUCHTH_0 + 2 * i, 25)
 
 # Go to sleep
 touchInt.deinit()
